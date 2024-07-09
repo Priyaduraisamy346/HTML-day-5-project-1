4d84db7d8b7e696f4e2e1130f86879175106ebb8
Priyaduraisamy346 <priyaduraisamy346@gmail.com>
2024-07-09 14:29:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('off').addEventListener('click',function(){document.getElementById("image").src="https://library.kissclipart.com/20180831/prw/kissclipart-glass-and-aluminum-vector-clipart-window-clip-art-601816896912a719.jpg"});
+document.getElementById('on').addEventListener('click',function(){document.getElementById("image").src="https://w7.pngwing.com/pngs/76/818/png-transparent-window-encapsulated-postscript-window-glass-furniture-building.png"});
+
+
